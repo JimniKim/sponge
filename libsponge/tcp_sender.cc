4e70ecd93bb80ed4e7581a98bfc277eea934fcc8
JimniKim <jimin5626@gmail.com>
2024-03-28 07:54:13 -0700

    timer 계속 true로 유지되도록

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -61,7 +61,7 @@ void TCPSender::fill_window()
         num = num - new_seg.header().fin;
 
         unsigned int min_num = min(min(_stream.buffer_size(),TCPConfig::MAX_PAYLOAD_SIZE -(new_seg.header().syn+ new_seg.header().fin)), num);
-        new_seg.payload() = Buffer (_stream.read(min_num)); // or = Buffer(string)
+        new_seg.payload() = Buffer (_stream.read(min_num)); 
         seq = seq + new_seg.length_in_sequence_space();
         num = num - new_seg.payload().str().size();
          _segments_out.push(new_seg);
@@ -102,7 +102,6 @@ void TCPSender::tick(const size_t ms_since_last_tick)
 {
     if ((ms_since_last_tick > rto) && timer)
     {
-        timer = false;
         _segments_out.push(outstanding_seg.front());
         if (_window_size != 0)
         {
