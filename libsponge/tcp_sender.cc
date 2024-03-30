452b0706e6b863f40632bde850b5a6cabdee2698
JimniKim <jimin5626@gmail.com>
2024-03-30 05:15:45 -0700

    _window_size == 0 && bytes_in_flight() !=0 인 경우엔 num =0

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -42,6 +42,8 @@ void TCPSender::fill_window()
 {
     
     size_t num = _window_size? _window_size - bytes_in_flight(): 1;
+    if (_window_size == 0 && bytes_in_flight() !=0)
+        num =0;
     //size_t curr_win = _window_size? _window_size - bytes_in_flight(): 1;
     //string buffer_string = _stream.peek_output(_stream.buffer_size());
     while (num > 0 && _fin == false)
