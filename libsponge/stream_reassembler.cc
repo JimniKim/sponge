ed274155b49cde007a597e663b9893052e9f82e1
JimniKim <jimin5626@gmail.com>
2024-04-25 07:25:27 -0700

    reassembler만!

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,11 +1,9 @@
 #include "stream_reassembler.hh"
 
-// Dummy implementation of a stream reassembler.
-
-// For Lab 1, please replace with a real implementation that passes the
-// automated checks run by `make check_lab1`.
-
-// You will need to add private members to the class declaration in `stream_reassembler.hh`
+#include <iostream>
+//! Ha Tran
+//! 2020-09-26
+//! 20aut CS144 Lab 1
 
 template <typename... Targs>
 void DUMMY_CODE(Targs &&... /* unused */) {}
@@ -13,85 +11,101 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 StreamReassembler::StreamReassembler(const size_t capacity)
-    : _output(capacity), _capacity(capacity), unassem_bytes(0), next(0), last_byte(0), unreassem(), _eof(false) {}
+    : _output(capacity)
+    , _capacity(capacity)
+    , unasmb()
+    , first_unasmb(0)
+    , n_unasmb_bytes(0)
+    , eof_set(false)
+    , last_byte(0) {}
+
+//! \details Look through existing unassembled chunks to
+//! 1) check if new chunk already contained in an existing chunk
+//!      -> return false (signal caller to ignore/skip over new chunk)
+//! 2) merge any existing chunks that overlaps into the new chunk
+//!      -> return true (caller can add to output byte stream or unassembled set)
+bool StreamReassembler::merge(std::list<Chunk> &chunks, Chunk &new_chunk) {
+    // [new0, new1) new chunk's byte number range
+    // [cur0, cur1) cur chunk's byte number ramge
+    const size_t new0 = new_chunk.index;
+    const size_t new1 = new_chunk.index + new_chunk.data.size();
+
+    // if new chunk of data overlaps with any existing unassembled chunk, merge them
+    auto it = chunks.begin();
+    while (it != chunks.end()) {
+        const size_t cur0 = it->index;
+        const size_t cur1 = it->index + it->data.size();
+        if (cur0 <= new0 && new1 <= cur1) {
+            // new data already contained in an existing chunk, exit early
+            return false;
+        } else if (new0 <= cur0 && cur1 <= new1) {
+            // new chunk encapsulates an existing chunk, remove to replace w/ new
+            n_unasmb_bytes -= it->data.size();
+            it = chunks.erase(it);
+        } else if (cur0 <= new0 && new0 <= cur1) {
+            // new chunk overlaps end of current chunk, prepend cur chunk's data to new data
+            new_chunk.data = it->data.substr(0, new0 - cur0) + new_chunk.data;
+            new_chunk.index = cur0;
+            n_unasmb_bytes -= it->data.size();
+            it = chunks.erase(it);
+        } else if (new0 <= cur0 && cur0 <= new1) {
+            // new chunk overlaps beginning of cur chunk, append
+            new_chunk.data += it->data.substr(new1 - cur0);
+            n_unasmb_bytes -= it->data.size();
+            it = chunks.erase(it);
+        } else {
+            // no overlap
+            it++;
+        }
+    }
+    return true;
+}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    size_t new_index = index;
-    string temp;
-    string _data;
-
+    if (_output.input_ended())
+        return;
     if (eof) {
-        last_byte = new_index;
-        _eof = true;
+        eof_set = true;
+        last_byte = index + data.size();
     }
 
-    if (index < next + _output.remaining_capacity()) {
-        _data = data.substr(0, min(data.size(), next + _output.remaining_capacity() - index));
-        if (eof && _data != data)
-            _eof = false;
-    } else
-        return;
+    // some part of new chunk of data is within the receiver advertised window
+    const size_t first_unacceptable = first_unasmb + _capacity - _output.buffer_size();
+    if (index + data.size() >= first_unasmb && index < first_unacceptable) {
+        Chunk new_chunk = {data, index};
 
-    if (index < next && index + _data.size() > next)  // partially overlapping
-    {
-        _data = _data.substr(next - index);
-        new_index = next;
-        if (eof)
-            last_byte = new_index;
-        // next = next + _output.write(_data.substr(next-index));
-        // if (index == last_byte && _eof)
-        //        _output.end_input();
-    } else if (index < next && index + _data.size() <= next)  // totally overlapping
-        return;
+        // if data too long to fit in window, chop off back bits
+        if (new_chunk.index + new_chunk.data.size() > first_unacceptable) {
+            new_chunk.data = new_chunk.data.substr(0, first_unacceptable - new_chunk.index);
+        }
+        // if data overlaps ByteStream, chop off front bits
+        if (new_chunk.index < first_unasmb) {
+            new_chunk.data = new_chunk.data.substr(first_unasmb - new_chunk.index);
+            new_chunk.index = first_unasmb;
+        }
 
-    temp = _data;
-
-    for (auto a = unreassem.begin(); a != unreassem.end();) {
-        if (a->first <= new_index && a->first + a->second.size() >= new_index + temp.size())
+        // if new chunk is already contained in an existing chunk, return early
+        if (not merge(unasmb, new_chunk))
             return;
 
-        if (a->first <= new_index && new_index <= a->first + a->second.size() &&
-            new_index + temp.size() > a->first + a->second.size()) {
-            temp = a->second.substr(0, new_index - a->first) + temp;
-            unassem_bytes = unassem_bytes - a->second.size();
-            new_index = a->first;
-            unreassem.erase(a++);
-
-        } else if (a->first <= new_index + temp.size() && new_index + temp.size() <= a->first + a->second.size() &&
-                   new_index < a->first) {
-            temp = temp.substr(0, a->first - new_index) + a->second;
-            new_index = new_index;
-            if (a->first == last_byte)
-                last_byte = new_index;
-            unassem_bytes = unassem_bytes - a->second.size();
-            unreassem.erase(a++);
-        } else if (a->first >= new_index && a->first + a->second.size() <= new_index + temp.size()) {
-            temp = temp;
-            unassem_bytes = unassem_bytes - a->second.size();
-            new_index = new_index;
-            unreassem.erase(a++);
-
-        } else
-            ++a;
+        // if can be assembled, write directly to ByteStream, else add to unasmb
+        if (new_chunk.index == first_unasmb) {
+            _output.write(new_chunk.data);
+            first_unasmb += new_chunk.data.size();
+        } else {
+            n_unasmb_bytes += new_chunk.data.size();
+            unasmb.push_back(new_chunk);
+        }
     }
-    unreassem.insert({new_index, temp});
-    unassem_bytes = unassem_bytes + temp.size();
 
-    if (unreassem.find(next) == unreassem.end())
-        return;
-    else {
-        size_t end = next;
-        next = next + _output.write(unreassem[next]);
-        unassem_bytes = unassem_bytes - unreassem[end].size();
-        if (end == last_byte && _eof)
-            _output.end_input();
-        unreassem.erase(end);
-    }
+    // after potentially updating _output ByteStream, check if we're done writing
+    if (eof_set && last_byte <= first_unasmb)
+        _output.end_input();
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return unassem_bytes; }
+size_t StreamReassembler::unassembled_bytes() const { return n_unasmb_bytes; }
 
-bool StreamReassembler::empty() const { return unreassem.empty(); }
+bool StreamReassembler::empty() const { return unasmb.empty(); }
--- libsponge/tcp_sender.hh
+++ libsponge/tcp_sender.hh
@@ -7,7 +7,7 @@
 #include "wrapping_integers.hh"
 
 #include <functional>
-#include <list>
+#include <map>
 #include <queue>
 
 //! \brief The "sender" part of a TCP implementation.
@@ -18,21 +18,6 @@
 //! segments if the retransmission timer expires.
 class TCPSender {
   private:
-    //! Retransmission timer
-    struct Timer {
-        size_t time_elapsed;
-        size_t timeout;
-
-        //! return true if time elapsed exceeds the timeout we set
-        bool expired() { return time_elapsed >= timeout; }
-
-        //! start a timer that expires after new_timeout milliseconds
-        void start(size_t new_timeout) {
-            time_elapsed = 0;
-            timeout = new_timeout;
-        }
-    };
-
     //! our initial sequence number, the number for our SYN.
     WrappingInt32 _isn;
 
@@ -48,29 +33,17 @@ class TCPSender {
     //! the (absolute) sequence number for the next byte to be sent
     uint64_t _next_seqno{0};
 
-    //! the (absolute) sequence number of the first byte the receiver has received
-    uint64_t _abs_ackno{0};
-
-    //! window size the receiver is expecting
-    size_t _window_size{1};
-
-    //! segment with their corresponding seqno
-    struct OrderedSegment {
-        uint64_t seqno;
-        TCPSegment segment;
-    };
-
-    //! segments sent but not yet acknowledged by receiver, sorted by absolute seqno
-    std::list<OrderedSegment> _outstanding_segments;
-
-    //! retransmission timer
-    Timer _timer;
-
-    //! number of times we've sent the same segment
-    size_t _n_consec_retransmissions{0};
-
-    //! enable cout statements for debugging
-    bool debug{false};
+    map<uint64_t, TCPSegment> outstanding_seg{};  // store outstanding segments
+    size_t _window_size;
+    WrappingInt32 _ackno;
+    unsigned int consecutive_retran;
+    uint32_t seq;
+    unsigned int rto;
+    unsigned int time_passed;
+    bool timer;
+    bool start;
+    bool _fin;
+    uint64_t flight_bytes;
 
   public:
     //! Initialize a TCPSender
