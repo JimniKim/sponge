f0224d86fb22b63c5eebb27da5aaa51a5d4cbd85
JimniKim <jimin5626@gmail.com>
2024-04-25 12:43:43 -0700

    리시버, 리어셈블러 둧다 변경

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -12,30 +12,24 @@ using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
     if (seg.header().syn) {
+        syn_received = true;
         isn = seg.header().seqno;
-        syn = true;
     }
+    if (!syn_received)
+        return;
 
-    if (seg.header().fin) {
-        fin = true;
-        abs_seq_fin = unwrap(
-            seg.header().seqno + seg.length_in_sequence_space() - 1, isn, _reassembler.stream_out().bytes_written());
-    }
-    _reassembler.push_substring(
-        string(seg.payload().str()),
-        unwrap(seg.header().seqno - 1 * (!seg.header().syn), isn, _reassembler.stream_out().bytes_written()),
-        fin);
+    uint64_t abs_seqno = unwrap(seg.header().seqno, isn, _reassembler.stream_out().bytes_written() + 1);
+    // -1 from absolute sequence number if we have a SYN bit
+    _reassembler.push_substring(seg.payload().copy(), abs_seqno - !seg.header().syn, seg.header().fin);
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const {
-    uint64_t n = _reassembler.stream_out().bytes_written() + 1;
-    if (syn) {
-        if (fin && abs_seq_fin == n)
-            n = n + 1;
-
-        return wrap(n, isn);
-    } else
-        return nullopt;
+    // +1 if we've seen a SYN flag, +1 if we've read up to the FIN flag in the reassembler
+    if (syn_received) {
+        return wrap(_reassembler.stream_out().bytes_written() + syn_received + _reassembler.stream_out().input_ended(),
+                    isn);
+    }
+    return {};
 }
 
 size_t TCPReceiver::window_size() const { return _reassembler.stream_out().remaining_capacity(); }
