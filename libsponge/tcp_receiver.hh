ce3f0228bff21711de795649a4d0092bb584accd
JimniKim <jimin5626@gmail.com>
2024-04-25 06:06:44 -0700

    receiver 뜯어고침!!

--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -6,7 +6,9 @@
 #include "tcp_segment.hh"
 #include "wrapping_integers.hh"
 
+#include <iostream>
 #include <optional>
+#include <string>
 
 //! \brief The "receiver" part of a TCP implementation.
 
@@ -19,18 +21,15 @@ class TCPReceiver {
 
     //! The maximum number of bytes we'll store.
     size_t _capacity;
-
+    bool syn_received;
     WrappingInt32 isn;
-    bool syn, fin;
-    uint64_t abs_seq_fin;
 
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity)
-        : _reassembler(capacity), _capacity(capacity), isn(0), syn(false), fin(false), abs_seq_fin(0) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity), syn_received(false), isn(0) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
