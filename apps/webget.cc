b3432b5fe75e7ec25f9437d258497e268392f1e7
JimniKim <jimin5626@gmail.com>
2024-04-25 00:22:59 -0700

    cerr 삽입

--- apps/webget.cc
+++ apps/webget.cc
@@ -40,8 +40,8 @@ void get_URL(const string &host, const string &path) {
     }
 
     tcp.close();
-    //cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    //cerr << "Warning: get_URL() has not been implemented yet.\n";
+    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
