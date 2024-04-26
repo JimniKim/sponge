ac040d4ee0c77e431d3bfa2cec5373d18d21a68f
JimniKim <jimin5626@gmail.com>
2024-04-25 18:13:21 -0700

    ackno 수정

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,31 +11,33 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    if (seg.header().syn) {
+    
+    if (!syn && !seg.header().syn)
+        return;
+    else if (seg.header().syn) 
+    {
         isn = seg.header().seqno;
         syn = true;
     }
 
-    if (seg.header().fin) {
-        fin = true;
-        abs_seq_fin = unwrap(
-            seg.header().seqno + seg.length_in_sequence_space() - 1, isn, _reassembler.stream_out().bytes_written());
-    }
+    
+        
+  
     _reassembler.push_substring(
-        string(seg.payload().str()),
+        seg.payload().copy(),
         unwrap(seg.header().seqno - 1 * (!seg.header().syn), isn, _reassembler.stream_out().bytes_written()),
-        fin);
+        seg.header().fin);
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const {
-    uint64_t n = _reassembler.stream_out().bytes_written() + 1;
-    if (syn) {
-        if (fin && abs_seq_fin == n)
-            n = n + 1;
+    if (!syn) 
+        return {};
 
-        return wrap(n, isn);
-    } else
-        return nullopt;
+    uint64_t n = _reassembler.stream_out().bytes_written() + 1;
+    if (_reassembler.stream_out().input_ended())
+            n = n + 1;
+    return wrap(n, isn);
+    
 }
 
 size_t TCPReceiver::window_size() const { return _reassembler.stream_out().remaining_capacity(); }
