e91a7eb572027736194c53c9f457d11c5f4957a3
JimniKim <jimin5626@gmail.com>
2024-04-25 17:12:12 -0700

    ..

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -13,7 +13,7 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 StreamReassembler::StreamReassembler(const size_t capacity)
-    : _output(capacity), _capacity(capacity), unassem_bytes(0), next(0), last_byte(0), unreassem(), _eof(false) {}
+    : _output(capacity), _capacity(capacity) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
